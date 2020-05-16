58f0d34dad86731aa60169a260eaa062005f64f3
Mr Uzo <chrisuzoewulu@gmail.com>
2020-05-16 17:07:55 -0400

    user password reset

--- snm/urls.py
+++ snm/urls.py
@@ -40,7 +40,19 @@ urlpatterns = [
     path('uno/', admin.site.urls),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}),
     path('tinymce/', include('tinymce.urls')),
-    path('ajax/validate_username/', validate_username, name='validate_username')
+    path('ajax/validate_username/', validate_username, name='validate_username'),
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(template_name='snmov/password_reset.html'),
+         name='password_reset'),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(template_name='snmov/password_reset_done.html'),
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='snmov/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='snmov/password_reset_complete.html'),
+         name='password_reset_complete'),
 ]
 
 if settings.DEBUG:
