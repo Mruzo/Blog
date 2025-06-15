94ba297b3dc1ad65a38596a98ff98486bc40cffe
chris <chrisuzoewulu@gmail.com>
2025-06-15 19:05:07 -0400

    static root in pro

--- snm/settings/pro.py
+++ snm/settings/pro.py
@@ -44,4 +44,5 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_LOCATION = 'static'
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
+STATIC_ROOT = None  # Override base.py setting
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
