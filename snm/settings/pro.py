5996d686c41ba18e57c7b4089bd252433f41a7ed
chris <chrisuzoewulu@gmail.com>
2025-06-18 23:00:32 -0400

    s3boto3vss3staticstorage

--- snm/settings/pro.py
+++ snm/settings/pro.py
@@ -47,7 +47,8 @@ AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_QUERYSTRING_AUTH = False
 
 # Static files configuration
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 
 # Media files configuration
